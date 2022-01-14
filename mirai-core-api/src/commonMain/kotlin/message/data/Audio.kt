b769afdcb2b1a134c78f86d8a54f7f21f42e3523
Him188 <Him188@mamoe.net>
2022-01-14 19:40:01 +0000

    Add `MessageVisitor`, `Message.accept` and `Message.acceptChildren` as internal API

--- mirai-core-api/src/commonMain/kotlin/message/data/Audio.kt
+++ mirai-core-api/src/commonMain/kotlin/message/data/Audio.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -20,6 +20,7 @@ import net.mamoe.mirai.contact.AudioSupported
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.message.MessageSerializers
 import net.mamoe.mirai.message.data.MessageChain.Companion.serializeToJsonString
+import net.mamoe.mirai.message.data.visitor.MessageVisitor
 import net.mamoe.mirai.utils.*
 
 /**
@@ -99,6 +100,11 @@ public sealed interface Audio : MessageContent {
      */
     public override fun toString(): String
     public override fun contentToString(): String = "[语音消息]"
+
+    @MiraiInternalApi
+    override fun <D, R> accept(visitor: MessageVisitor<D, R>, data: D): R {
+        return visitor.visitAudio(this, data)
+    }
 }
 
 
--- mirai-core-api/src/commonMain/kotlin/message/data/Voice.kt
+++ mirai-core-api/src/commonMain/kotlin/message/data/Voice.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -17,6 +17,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.message.data.visitor.MessageVisitor
 import net.mamoe.mirai.utils.*
 
 
@@ -172,6 +173,11 @@ public open class Voice @MiraiInternalApi constructor(
         result = 33 * result + _codec
         result = 15 * result + _url.hashCode()
         return result
+    }
+
+    @MiraiInternalApi
+    override fun <D, R> accept(visitor: MessageVisitor<D, R>, data: D): R {
+        return visitor.visitVoice(this, data)
     }
 }
 
